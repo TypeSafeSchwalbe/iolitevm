17eaa2d40c3c757b3457a3c8660d433afd0efc76
TypeSafeSchwalbe <78918080+TypeSafeSchwalbe@users.noreply.github.com>
2023-07-16 10:19:41 +0300

    Add files via upload

--- testdlib/src/lib.c
+++ testdlib/src/lib.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 #include "../../iolitevm_api.h"
 
-IoliteValue println_f32(IoliteValue* args) {
-    printf("%f\n", args[0].value.f32);
+IoliteValue println_flt(IoliteValue* args) {
+    printf("%f\n", args[0].value.flt);
     return IoliteUnitValue;
 }
 
